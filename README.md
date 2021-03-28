9d858e8ad2f60151587836d1b8113b0af26bf9ea
Adrian Alexander Eriksen <adrianalexeriksen@gmail.com>
2021-03-28 10:41:55 +0200

    Add favicon

---
+++ README.md
@@ -0,0 +1,6 @@
+# Photographic
+
+## Credits
+
+Favicon graphics created by Twitter licensed under CC-BY 4.0.
+https://github.com/twitter/twemoji
--- config/settings/base.py
+++ config/settings/base.py
@@ -91,4 +91,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [APPS_DIR / "static"]
+
 STATIC_URL = "/static/"

eb0b0ad994937fb5d0cb17c61c83d1bca77edbc1
Adrian Alexander Eriksen <adrianalexeriksen@gmail.com>
2021-05-10 19:13:19 +0200

    Redirect user to next page when parameter is present Resolves #4

--- photographic/users/tests/test_views.py
+++ photographic/users/tests/test_views.py
@@ -1,4 +1,6 @@
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth.views import LoginView
+from django.test import RequestFactory, TestCase
 from django.urls import reverse
 
 from photographic.users.models import User
@@ -11,3 +13,22 @@ class TestIndexView(TestCase):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, '<a href="/u/jonas/">jonas</a>')
+
+
+class TestLoginView(TestCase):
+    def setUp(self):
+        self.rf = RequestFactory()
+
+    def test_redirects_to_correct_path(self):
+        next_page = "/example/path/"
+        endpoint = "/accounts/login/?next=" + next_page
+
+        request = self.rf.get(endpoint)
+        request.user = AnonymousUser()
+
+        response = LoginView.as_view()(request)
+
+        expected_form_action = f'action="{endpoint}"'
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, expected_form_action)

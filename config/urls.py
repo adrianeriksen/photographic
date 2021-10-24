706fd9f9fed08bf10132ed45839c9a97194a4971
Adrian Alexander Eriksen <adrianalexeriksen@gmail.com>
2021-10-24 13:48:02 +0200

    Write out routes

--- config/urls.py
+++ config/urls.py
@@ -9,8 +9,8 @@ from photographic.photos.views import ListView as HomeView
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
-    path("p/", include("photographic.photos.urls")),
-    path("u/", include("photographic.users.urls")),
+    path("photos/", include("photographic.photos.urls")),
+    path("users/", include("photographic.users.urls")),
     path("cookie-policy/", CookiePolicy.as_view(), name="cookie-policy"),
     path("privacy-policy/", PrivacyPolicy.as_view(), name="privacy-policy"),
     path("", HomeView.as_view(), name="home")

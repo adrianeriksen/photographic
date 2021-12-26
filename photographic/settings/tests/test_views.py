15668cbebecaba1ff6f89371f264df6c4df4c795
Adrian Alexander Eriksen <adrianalexeriksen@gmail.com>
2021-12-26 16:14:31 +0100

    Fix failing password change test

--- photographic/settings/tests/test_views.py
+++ photographic/settings/tests/test_views.py
@@ -1,18 +1,11 @@
-from django.contrib.sessions.middleware import SessionMiddleware
 from django.test import RequestFactory, TestCase
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 
 from photographic.users.models import Profile, User
-from photographic.settings.views import UpdatePasswordView, UpdateProfileView
+from photographic.settings.views import UpdateProfileView
 
 
 class TestUpdatePasswordView(TestCase):
-    def setUp(self):
-        self.rf = RequestFactory()
-
-    def test_user_is_deauthenticated_after_password_change(self):
-        pass
-
     def test_user_is_redirected_to_login_page(self):
         user = User.objects.create_user("adrian", email="adrian@example.com", password="password")
 
@@ -22,16 +15,8 @@ class TestUpdatePasswordView(TestCase):
             "new_password2": "securepassword"
         }
 
-        request = self.rf.post("/example/path", data)
-        request.user = user
-        request._dont_enforce_csrf_checks = True
-
-        session_middleware = SessionMiddleware()
-        session_middleware.process_request(request)
-
-        request.session.save()
-
-        response = UpdatePasswordView.as_view()(request)
+        self.client.force_login(user)
+        response = self.client.post(reverse("settings:password"), data)
 
         login_url = reverse_lazy("login")
         self.assertEqual(response.status_code, 302)

0106d7c689a81e6c1e63d25d39cb05bd384ca462
Adrian Alexander Eriksen <adrian@adrianeriksen.no>
2023-04-16 10:04:51 +0200

    Remove static pages

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import include, path
 
-from photographic.pages.views import CookiePolicy, PrivacyPolicy, Up
 from photographic.photos.views import ListView as HomeView
+
+
+def up(request):
+    return HttpResponse("Cleared for takeoff!")
+
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
@@ -12,8 +17,6 @@ urlpatterns = [
     path("settings/", include("photographic.settings.urls")),
     path("photos/", include("photographic.photos.urls")),
     path("users/", include("photographic.users.urls")),
-    path("cookie-policy/", CookiePolicy.as_view(), name="cookie-policy"),
-    path("privacy-policy/", PrivacyPolicy.as_view(), name="privacy-policy"),
-    path("up/", Up.as_view()),
+    path("up/", up),
     path("", HomeView.as_view(), name="home")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
